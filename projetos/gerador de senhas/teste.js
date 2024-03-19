e67fd208b4344e1453258ddd7cfa0064a278d06c
Mário <marionobre1111@gmail.com>
2024-03-19 17:15:56 -0300

    gerador de senha iniciado

---
+++ projetos/gerador de senhas/teste.js
@@ -0,0 +1,14 @@
+function generatePassword() {
+    const chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]"
+
+    let password = ''
+
+    for (let i = 0; i < 8; i++) {
+        const randomNumber = Math.floor(Math.random() * chars.length)
+        password += chars.substring(randomNumber, randomNumber + 1)
+
+        console.log(password)
+    }
+}
+
+generatePassword()
